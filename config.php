c66274663f8226658fa8092739c3b5be5b7db0b3
TuM4N <49667787+TuM4N@users.noreply.github.com>
2019-04-17 12:23:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// HARUS DISI DENGAN BENAR
+// KEMUNGKINAN NGGAK WORK KECIL JIKA MELIHAT VIDEONYA DENGAN BENAR
+// ISI BAGIAN xxxxxxxxxxxxxxxxxxxxxxx SAJA ";
+// CONTOH = "voyager-api-key: p5DqGsYcOFucDadCvfWyjgrbk3Bs1RE1";
+$headers = array();
+$headers[] = "voyager-api-key: xxxxxxxxxxxxxxxxxxxxxxx";
+$headers[] = "buzzbreak-api-key: xxxxxxxxxxxxxxxxxxxxxxx";
+$headers[] = "buzzbreak-client: android";
+$headers[] = "buzzbreak-device-id: xxxxxxxxxxxxxxxxxxxxxxx";
+$headers[] = "buzzbreak-locale: in_ID";                                                                                   
+$headers[] = "buzzbreak-app-version: xxxx";
+$headers[] = "buzzbreak-account-id: xxxxxx";
+$headers[] = "content-type: application/json; charset=utf-8";                                                            
+$headers[] = "content-length: 29";
+$headers[] = "user-agent: okhttp/3.11.0";
+// KEMUNGKINAN NGGAK WORK KECIL JIKA MELIHAT VIDEONYA DENGAN BENAR
+// SEMOGA MEMBANTU
+
+?>
